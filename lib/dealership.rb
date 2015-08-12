bc08c29833eacd15ad69386caaf48e0d73899e96
Kyle Mellander & Michael Bunker <student@epicodus.com>
2015-08-12 10:08:49 -0700

    intialize dealership class with name method.

---
+++ lib/dealership.rb
@@ -0,0 +1,14 @@
+class Dealership
+
+  @@dealership = []
+  @@id_count = 0
+
+  attr_reader :name, :id
+
+  def initialize(name)
+    @name = name
+    @id = @@id_count + 1
+    @@id_count += 1
+  end
+
+end
